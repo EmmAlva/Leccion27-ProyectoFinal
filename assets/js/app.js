04c8404ce3d9f704249133709a3c451b7ee9ffe1
Emma Tapia <clioemma_28@hotmail.com>
2017-05-02 17:43:26 -0500

    change header

---
+++ assets/js/app.js
@@ -0,0 +1,17 @@
+var lastScrollTop = 0;
+	window.addEventListener("scroll",function(){
+		// var imgPink=document.getElementById("img-pink");
+		// var imgWhite = document.getElementById("img-white");
+		if(document.body.bodyScrollTop > 5 || document.documentElement.bodyScrollTop > 5){
+			document.getElementById("header").className = "run";
+			// imgPink.style.display = "block";
+			// imgWhite.style.display = "none";
+
+		}else{
+			document.getElementById("header").className ="";
+			// imgWhite.style.display = "block";
+			// imgPink.style.display  ="none";
+
+		}
+		
+	});
